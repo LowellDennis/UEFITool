68074ee112cc5e50ee6461c3fcf8d3895bfb5216
Lowell Dennis <lowell.dennis@hpe.com>
2023-07-24 17:13:14 -0500

    Program that checks that maximum debug has output for every line

---
+++ checkit.py
@@ -0,0 +1,41 @@
+import os
+import sys
+
+Line = [0]
+
+def CheckIt(file):
+    count = 0
+    with open(file, 'r') as f:
+        while True:
+            line = f.readline()
+            if not line: break
+            count += 1
+            if line.startswith('v'):          continue
+            if line.startswith('Parsing'):    continue
+            if line.startswith('-----'):      continue
+            if line.startswith('Processing'):
+                Line[-1] = 0
+                continue
+            # Line should start with a line number
+            try:
+                number, rest = line.split(':', maxsplit=1)
+                number = int(number)
+                # Number should be one more than last line number
+                if number == Line[-1] + 1: Line[-1] += 1
+                # Look for special cases where line number does not change
+                elif number == Line[-1]:
+                    if not rest.split()[0] in ['ConvertedCondition', 'ConditionalLevel', 'Limiting']:
+                        if not rest.startswith('[') and rest.rstrip().endswith(']'):
+                            print(f'Unexpected line numbering discontinuity found at line {count}')
+                # Look for specail case where line number can change dramatically
+                elif not 'Previously' in rest and not 'Returning' in rest:
+                    print(f'Unexpected line numbering discontinuity found at line {count}')
+                # Look for include
+                if 'Including' in rest: Line.append(0)
+                # Look for return
+                elif 'Returning' in rest: Line.pop()
+            except ValueError:
+                if "RESULTS:" in line: break
+                print(f'Unexpected line format found at line {count}')
+
+CheckIt("u54")
