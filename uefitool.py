fd83d031180663ce8e832b9de91feef85fd0366b
Lowell Dennis <lowell.dennis@hpe.com>
2023-08-16 11:35:06 -0500

    Removed todos

--- uefitool.py
+++ uefitool.py
@@ -2950,8 +2950,6 @@ PlatformInfo(platform.replace('\\', '/'))
 ###########
 ### TBD ###
 ###########
-# - With references added, reference withing INF and DEC can be removed
-# - Convert other file lists to dictionaries and used MacroVer like it is used for DSC?
 # - Cross-reference items to make sure things are consistent?
 # - Generate files instead of output to the screen so it can be used by other utilites
 # - Fully check syntax of files!
