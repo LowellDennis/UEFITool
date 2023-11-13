a67c50a3fe7ebda16d99bf135cb3ac95b1c6abfc
Lowell Dennis <lowell.dennis@hpe.com>
2023-11-13 13:42:38 -0600

    Preparing for release of V0.7

--- globals.py
+++ globals.py
@@ -12,7 +12,7 @@ from debug import DebugLevel
 ###################
 # Program version #
 ###################
-ProgramVersion = 0.6
+ProgramVersion = 0.7
 
 ################################
 # Regular expression constants #
